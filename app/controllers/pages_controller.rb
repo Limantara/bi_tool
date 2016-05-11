757aa9f92a6d234c24639910745f2c3f2e171dfb
Limantara <edwin.limantara@sjsu.edu>
2016-05-10 20:17:03 -0700

    Add initial commit

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,28 @@
+require 'net/http'
+
+class PagesController < ActionController::Base
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  protect_from_forgery with: :exception
+
+  def index
+		@table = get_table
+  end
+
+  def charts
+  end
+
+  def tables
+		@table = get_table
+  end
+
+private
+	def get_table
+		url = URI.parse('http://localhost:8080/api')
+  	req = Net::HTTP::Get.new(url.to_s)
+		res = Net::HTTP.start(url.host, url.port) {|http|
+		  http.request(req)
+		}
+		res.body
+	end	
+end
